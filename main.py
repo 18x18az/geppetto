37d98c20786b3fece60c068b8cbbf4bd0f152716
Alec Miller <alec@18x18az.org>
2023-10-20 19:39:04 -0700

    feat: connects to web browser display

--- main.py
+++ main.py
@@ -1,12 +1,52 @@
 from getUuid import get_uuid
 from connect import get_server
 from register import register
+import subprocess
 
-server = get_server()
+server, serverPort, browserPort = get_server()
 
 ident = get_uuid()
 
-register(server, ident)
+## Run command chromium-browser http://{server}:{browserPort}/display/field/{ident}
+urlToOpen = 'http://' + server + ':' + str(browserPort) + '/display/field/' + ident
+## Start x server
+# ##   --window-size=1920,1080 \
+#   --window-position=0,0 \
+#   --start-fullscreen \
+#   --kiosk \
+#   --incognito \
+#   --noerrdialogs \
+#   --disable-translate \
+#   --no-first-run \
+#   --fast \
+#   --fast-start \
+#   --disable-infobars \
+#   --disable-features=TranslateUI \
+#   --disk-cache-dir=/dev/null \
+#   --overscroll-history-navigation=0 \
+#   --disable-pinch
+subprocess.Popen(['chromium-browser', urlToOpen,
+                  '--window-size=1920,1080',
+                  '--window-position=0,0',
+                  '--start-fullscreen',
+                  '--kiosk',
+                  '--incognito',
+                  '--noerrdialogs',
+                  '--disable-translate',
+                  '--no-first-run',
+                  '--fast',
+                  '--fast-start',
+                  '--disable-infobars',
+                  '--disable-features=TranslateUI',
+                  '--disk-cache-dir=/dev/null',
+                  '--overscroll-history-navigation=0',
+                  '--disable-pinch']
+                  )
+
+register(server, serverPort, ident)
 
 print(server)
 print(ident)
+
+while True:
+    pass
