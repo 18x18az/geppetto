c20228e8fa1af1f21846723703cf9d50cfe4089b
Alec Miller <alecmichaelmiller@gmail.com>
2024-01-26 15:45:33 -0700

    display stuff

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from getUuid import get_uuid
 from connect import get_server
 from register import getFieldInfo
 import subprocess
-#from fieldCon import doTheThing
 
 # ## Run command chromium-browser http://{server}:{browserPort}/display/field/{ident}
 # urlToOpen = 'http://' + server + ':' + str(browserPort) + '/display/field/' + ident
@@ -47,3 +46,21 @@ if __name__ == "__main__":
     server, serverPort, browserPort = get_server()
     ident = get_uuid()
     getFieldInfo(server, serverPort, ident)
+    urlToOpen = 'http://' + server + ':' + str(browserPort) + '/display/field/' + ident
+    subprocess.call(['chromium-browser', urlToOpen,
+                  '--window-size=1920,1080',
+                  '--window-position=0,0',
+                  '--start-fullscreen',
+                  '--kiosk',
+                  '--incognito',
+                  '--noerrdialogs',
+                  '--disable-translate',
+                  '--no-first-run',
+                  '--fast',
+                  '--fast-start',
+                  '--disable-infobars',
+                  '--disable-features=TranslateUI',
+                  '--disk-cache-dir=/dev/null',
+                  '--overscroll-history-navigation=0',
+                  '--disable-pinch']
+                  )
--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 cp .xinitrc ~/.xinitrc
+cp .bash_profile ~/.bash_profile
 sudo apt update -qq
 sudo apt install --no-install-recommends -y xserver-xorg-video-all xserver-xorg-input-all xserver-xorg-core xinit x11-xserver-utils cockpit python3 python3-pip python3-dev chromium-browser unclutter
 sudo cp ./control.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable --now control
 sudo systemctl enable --now cockpit.socket
-pip3 install "gql[all]"
-pip3 install pyusb
+sudo pip3 install "gql[all]"
+sudo pip3 install pyusb
 sudo cp ./1-field.rules /etc/udev/rules.d
 sudo groupadd dialout
 sudo usermod -aG dialout admin
